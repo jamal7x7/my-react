da28774461a5d405ec7d383b51ec62cd60eff06e
Jamal Rayan <jamal7x7@google.com>
2022-02-04 18:36:25 +0000

    added the split brtween vanilla and my-ract app

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const vanillaApp = document.querySelector<HTMLDivElement>('#vanilla-app')
+const myReactApp = document.querySelector<HTMLDivElement>('#my-react-app')
+
+myReactApp.innerHTML = ` <h3>My React Clock</h3> <hr/>`
 
 let clock
 
 function render() {
   const template = (n) => `
+
     <div> Clock #${n} </div>
-  <h4>time now is</h4>
-  <h1 class="clock">${clock}</h1>
-  <hr/>
+    <h4>time now is</h4>
+    <h1 class="clock">${clock}</h1>
+    <hr/>
+
 `
 
-  const crateTemplate = (n) =>
-    new Array(n).fill(0).reduce((acc, _, i) => {
+  const createTemplate = (n) =>
+    new Array(n).fill(null).reduce((acc, _, i) => {
       acc += template(i)
       return acc
     }, ` <h3>Vanilla Clock</h3> <hr/>`)
 
-  app.innerHTML = crateTemplate(10)
-
-  // return arr.map((r) => {
-  //   // console.log(clock)
-  //   return r + r
-  // })
+  vanillaApp.innerHTML = createTemplate(10)
 }
 
 setInterval(() => {
